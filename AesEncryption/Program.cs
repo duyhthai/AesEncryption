5228220881cf68df69aa9748b991ec52e3a30d63
Duy Thai <duy.thai@altsourcesoftware.com>
2020-12-04 18:15:13 +0700

    init solution

---
+++ AesEncryption/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AesEncryption
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			EncryptFile("ThePasswordToDecryptAndEncryptTheFile");
+		}
+
+		private static void EncryptFile(string password)
+		{
+			// For additional security Pin the password of your files
+			GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
+
+			// Encrypt the file
+			FileEncryption.FileEncrypt(@"test.png", password);
+
+			// To increase the security of the encryption, delete the given password from the memory !
+			FileEncryption.ZeroMemory(gch.AddrOfPinnedObject(), password.Length * 2);
+			gch.Free();
+
+			// You can verify it by displaying its value later on the console (the password won't appear)
+			Console.WriteLine("The given password is surely nothing: " + password);
+		}
+
+		private static void DecryptFile()
+		{
+			string password = "ThePasswordToDecryptAndEncryptTheFile";
+
+			// For additional security Pin the password of your files
+			GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
+
+			// Decrypt the file
+			FileEncryption.FileDecrypt(@"test.png.aes", @"test_decrypted.png", password);
+
+			// To increase the security of the decryption, delete the used password from the memory !
+			FileEncryption.ZeroMemory(gch.AddrOfPinnedObject(), password.Length * 2);
+			gch.Free();
+
+			// You can verify it by displaying its value later on the console (the password won't appear)
+			Console.WriteLine("The given password is surely nothing: " + password);
+		}
+	}
+}
